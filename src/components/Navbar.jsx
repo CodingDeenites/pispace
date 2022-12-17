34d5892c961e5ae97185cbee45b72cdac66780d6
neerav gupta <neeravgupta07@gmail.com>
2022-12-17 15:07:52 +0530

    sd

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,30 @@
 import React from 'react';
 import '../main.css'
-import { Button, ButtonGroup, Box, Image } from '@chakra-ui/react'
+import { Button, ButtonGroup, Box, Image, Stack, Spacer, Flex } from '@chakra-ui/react'
 import { auth } from "../../firebase.config";
 import { useSignInWithGoogle } from "react-firebase-hooks/auth";
 import { useNavigate } from "react-router-dom";
 
-export const Navbar = () => {
+export const Navbar = ({ title, desc, ...rest }) => {
     const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
     let navigate = useNavigate();
     return (
-        <div className='Navbar'>
-                <Image src='https://cdn.discordapp.com/attachments/1038360446383693904/1038361636211273728/pi_space_png.png' alt='Dan Abramov' className='navimg' />
-                <Box alignItems='right'>
+        <Box className='Navbar'>
+            <Flex minWidth='max-content' alignItems='center' gap='2'maxHeight='max-content'>
+                <Box p='2'>
+                    <Image src='https://cdn.discordapp.com/attachments/1038360446383693904/1038361636211273728/pi_space_png.png' alt='logo' className='navimg' boxSize='50%' margin-left= '20px' margin-top='1vh' />    
+                </Box>
+                <Spacer />
+                    <Button colorScheme='blue'><a href="/login">Get started</a></Button>
+            </Flex>
+                {/* <Box alignItems='right'>
                     <Button >
                     <a href='/login' align='right'>Login</a>
                     </Button>
-                </Box>
-        </div>
+                </Box> */}
+
+        </Box>
     );
 };
+
+
